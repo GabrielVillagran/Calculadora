c60b4b4548a3f9b72b3a27b304806bbd1d250e93
Gabriel Villagrán <44887537+Villagran29A@users.noreply.github.com>
2021-04-15 23:07:29 -0500

    Add files via upload

---
+++ Calc_VillagránSaucedoCerdaEstrada/Calculadora/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.pantalla = new System.Windows.Forms.TextBox();
+            this.historial = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.b1.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(23, 304);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(56, 54);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.numClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button1.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(97, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "2";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.numClick);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button9.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(173, 304);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(56, 54);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "3";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.numClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button2.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(173, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 54);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "6";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.numClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button3.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(97, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 54);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "5";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.numClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button4.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(23, 225);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 54);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.numClick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button5.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(173, 143);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 54);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "9";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.numClick);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button6.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(97, 143);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(56, 54);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "8";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.numClick);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button7.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(23, 143);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 54);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.numClick);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button8.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(97, 376);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(56, 54);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "0";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.numClick);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button10.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(289, 376);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(56, 54);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "C";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.limpiarClick);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button11.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(289, 143);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(56, 54);
+            this.button11.TabIndex = 19;
+            this.button11.Text = "+";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.opClick);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button12.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(365, 143);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(56, 54);
+            this.button12.TabIndex = 20;
+            this.button12.Text = "-";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.opClick);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button13.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(289, 214);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(56, 54);
+            this.button13.TabIndex = 21;
+            this.button13.Text = "X";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.opClick);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button14.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.Location = new System.Drawing.Point(365, 214);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(56, 54);
+            this.button14.TabIndex = 22;
+            this.button14.Text = "/";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.opClick);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button15.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.Location = new System.Drawing.Point(289, 294);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(56, 54);
+            this.button15.TabIndex = 23;
+            this.button15.Text = "^";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.opClick);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button16.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button16.Location = new System.Drawing.Point(365, 294);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(56, 54);
+            this.button16.TabIndex = 24;
+            this.button16.Text = "\t√";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.raizClick);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button17.Font = new System.Drawing.Font("Orbitron", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button17.Location = new System.Drawing.Point(365, 376);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(56, 54);
+            this.button17.TabIndex = 25;
+            this.button17.Text = "=";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.igualClick);
+            // 
+            // pantalla
+            // 
+            this.pantalla.BackColor = System.Drawing.Color.PowderBlue;
+            this.pantalla.Font = new System.Drawing.Font("Orbitron", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pantalla.Location = new System.Drawing.Point(23, 87);
+            this.pantalla.Name = "pantalla";
+            this.pantalla.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.pantalla.Size = new System.Drawing.Size(397, 36);
+            this.pantalla.TabIndex = 26;
+            this.pantalla.Text = "0";
+            // 
+            // historial
+            // 
+            this.historial.AutoSize = true;
+            this.historial.Font = new System.Drawing.Font("Orbitron", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.historial.Location = new System.Drawing.Point(25, 62);
+            this.historial.Name = "historial";
+            this.historial.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.historial.Size = new System.Drawing.Size(0, 22);
+            this.historial.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(446, 454);
+            this.Controls.Add(this.historial);
+            this.Controls.Add(this.pantalla);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.TextBox pantalla;
+        private System.Windows.Forms.Label historial;
+    }
+}
+
---
+++ Calc_VillagránSaucedoCerdaEstrada/Calculadora/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        bool reslt;
+        bool operando = false;
+        string operador;
+        double num1;        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numClick(object sender, EventArgs e)
+        {
+            Button numero = (Button)sender;
+            if (pantalla.Text == "0")
+                pantalla.Clear();
+            if(reslt)
+            {
+                reslt = false;
+                pantalla.Clear();
+            }
+            pantalla.Text = pantalla.Text + numero.Text;
+            historial.Text = historial.Text + numero.Text;
+
+        }
+
+        private void opClick(object sender, EventArgs e)
+        {
+            Button op = (Button)sender;
+            if (operando)
+            {
+                igual(num1, Double.Parse(pantalla.Text), operador);
+                operador = op.Text;
+            }
+            else
+            {
+                operando = true;
+                num1 = Double.Parse(pantalla.Text);
+                pantalla.Clear();
+                operador = op.Text;
+                
+            }
+            historial.Text = historial.Text + op.Text;
+
+        }
+
+        private void raizClick(object sender, EventArgs e)
+        {
+            Button raiz = (Button)sender;
+            if(operando)
+            {
+                int l = historial.Text.Length;
+                igual(Double.Parse("0"+historial.Text[l-3]), Double.Parse("0"+historial.Text[l-1]),"" + historial.Text[l-2]);
+            }
+            this.num1 = Math.Sqrt(Double.Parse(pantalla.Text));
+            pantalla.Text = this.num1.ToString();
+            historial.Text = raiz.Text + "(" + historial.Text + ")";
+            operando = false;
+        }
+
+        private void igual(double num1, double num2, string operador)
+        {
+            switch(operador)
+            {
+                case "+":
+                    this.num1 = num1 + num2;
+                    pantalla.Text = this.num1.ToString();
+                    break;
+                case "-":
+                    this.num1 = num1 - num2;
+                    pantalla.Text = this.num1.ToString();
+                    break;
+                case "X":
+                    this.num1 = num1 * num2;
+                    pantalla.Text = this.num1.ToString();
+                    break;
+                case "/":
+                    if (num2 == 0)
+                        pantalla.Text = "0";
+                    else
+                    {
+                        this.num1 = num1 / num2;
+                        pantalla.Text = this.num1.ToString();
+                    }
+                    break;
+                case "^":
+                    this.num1 = Math.Pow(num1, num2);
+                    pantalla.Text = this.num1.ToString();
+                    break;
+            }
+            reslt = true;
+        }
+
+        private void igualClick(object sender, EventArgs e)
+        {
+            if (operando)
+            {
+                igual(num1, Double.Parse(pantalla.Text), operador);
+                operando = false;
+                historial.Text = "";
+            }
+            
+        }
+
+        private void limpiarClick(object sender, EventArgs e)
+        {
+            pantalla.Text = "0";
+            historial.Text = "";
+        }
+    }
+}
